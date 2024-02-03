f17bef437782bca099ab35607138b67773f27c6c
Mohammadarifcoding <minaraarifa@gmail.com>
2024-02-03 15:58:17 +0600

    Added the purchase page

--- src/Components/Pages/Purchase/Purchase.jsx
+++ src/Components/Pages/Purchase/Purchase.jsx
@@ -9,56 +9,56 @@ import Process from "./Process/Process";
 import UseCurrency from "../../../Hook/UseCurrency";
 import { useParams } from "react-router-dom";
 
-// const Purchase = () => {
-//   const [purchaseData, setPurchaseeData] = useState(
-//     JSON.parse(localStorage.getItem("purchase"))
-//   );
-//   const {currencyParams,currentWay} = useParams()
+const Purchase = () => {
+  const [purchaseData, setPurchaseeData] = useState(
+    JSON.parse(localStorage.getItem("purchase"))
+  );
+  const {currencyParams,currentWay} = useParams()
   
 
-//   const [currency,refetchCurrency] = UseCurrency()
+  const [currency,refetchCurrency] = UseCurrency()
 
-//   const [nextFrom,setNextForm] = useState(1)
+  const [nextFrom,setNextForm] = useState(1)
 
-//   const [address,setAddressSelected] = useState('')
-//   const [allTheitem,setAllTheItem] = useState(purchaseData)
+  const [address,setAddressSelected] = useState('')
+  const [allTheitem,setAllTheItem] = useState(purchaseData)
  
  
 
-//   return (
-//     <Container>
-//       <div className="lg:mt-20 mt-10 pb-40">
-//         <h2 className="lg:text-3xl text-2xl font-medium md:text-start text-center">
-//           {
-//             currentWay == 'Sell' ? <>Sell money</>:<>Order money</>
-//           }
+  return (
+    <Container>
+      <div className="lg:mt-20 mt-10 pb-40">
+        <h2 className="lg:text-3xl text-2xl font-medium md:text-start text-center">
+          {
+            currentWay == 'Sell' ? <>Sell money</>:<>Order money</>
+          }
           
-//         </h2>
+        </h2>
       
-//         <Process no={nextFrom}></Process>
-//          {
-//           nextFrom == 3 ? '':<OrderSummary allTheitem={allTheitem} setAllTheItem={setAllTheItem} purchaseData={purchaseData}></OrderSummary>
-//          }
+        <Process no={nextFrom}></Process>
+         {
+          nextFrom == 3 ? '':<OrderSummary allTheitem={allTheitem} setAllTheItem={setAllTheItem} purchaseData={purchaseData}></OrderSummary>
+         }
          
 
-//         {/* Add Currency */}
-//         { nextFrom == 1 ? <>
-//           <AddProduct currentWay={currentWay} currencyParams={currencyParams} allTheitem={allTheitem} setAllTheItem={setAllTheItem} purchaseData={purchaseData} setPurchaseeData={setPurchaseeData}></AddProduct>
-//         <ul className="flex flex-col font-medium mt-10 text-lg list-disc ml-6">
-//            <li className=" ">Bring you ID for collection</li>
-//            <li >Order online, pay in-store</li>
-//            <li>24-hour rate guarantee upon order arrival in the store</li>
-//         </ul>
-//         </>  :''
+        {/* Add Currency */}
+        { nextFrom == 1 ? <>
+          <AddProduct currentWay={currentWay} currencyParams={currencyParams} allTheitem={allTheitem} setAllTheItem={setAllTheItem} purchaseData={purchaseData} setPurchaseeData={setPurchaseeData}></AddProduct>
+        <ul className="flex flex-col font-medium mt-10 text-lg list-disc ml-6">
+           <li className=" ">Bring you ID for collection</li>
+           <li >Order online, pay in-store</li>
+           <li>24-hour rate guarantee upon order arrival in the store</li>
+        </ul>
+        </>  :''
       
-//       }
+      }
         
 
 
-//         <CheckingPoint nextFrom={nextFrom} setNextForm={setNextForm} setAddressSelected={setAddressSelected}></CheckingPoint>
-//       </div>
-//     </Container>
-//   );
-// };
+        <CheckingPoint nextFrom={nextFrom} setNextForm={setNextForm} setAddressSelected={setAddressSelected}></CheckingPoint>
+      </div>
+    </Container>
+  );
+};
 
 export default Purchase;
