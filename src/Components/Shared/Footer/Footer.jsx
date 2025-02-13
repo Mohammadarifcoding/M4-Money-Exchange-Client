555f0aa1c4dbea0ae999677a8319101f9e7027be
asiful islam <work.asifulislam@gmail.com>
2025-02-13 23:51:11 +0600

    footer text replaced

--- src/Components/Shared/Footer/Footer.jsx
+++ src/Components/Shared/Footer/Footer.jsx
@@ -93,11 +93,11 @@ const Footer = () => {
  </div>
 </div >
 {/* copyright Section */}
-<h2 className="text-white text-[15px] mt-16  mb-3 ">M4FX IS A TRADING NAME OF M4 TRADING LIMITED,REGISTERED IN ENGLAND AND WALES UNDER COMPANY NUMBER 10520708,REGISTERED ADDRESS:707 GREEN LANE, DAGENHAM, RM8 1UU
+<h2 className="text-white text-[15px] mt-16  mb-3 ">M4 Travels IS A TRADING NAME OF M4 TRADING LIMITED,REGISTERED IN ENGLAND AND WALES UNDER COMPANY NUMBER 10520708,REGISTERED ADDRESS:707 GREEN LANE, DAGENHAM, RM8 1UU
 Copyright © 2024 M4 All rights reserved.</h2>
 <div className="border-t-2">
 <div className=" px-20 py-4 flex justify-between ">
- <p>Copyright © 2024 M4 All rights reserved.</p>
+ <p>Copyright © 2025 M4 All rights reserved.</p>
  <div>
      <ul className="flex gap-x-4 text-xl items-center">
       <Link to={"https://www.facebook.com/share/1BUhEVKu3a/?mibextid=LQQJ4d"}>   <li><FaFacebookF/></li></Link>
